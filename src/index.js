cb070cddddb9da42a7433918130ae1c159aaf7ef
idiidk <dviide@protonmail.com>
2019-08-24 14:53:02 +0200

    Added generic adapter class as export

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import Helpers from './helpers';
 import Session from './session';
 import Events from './events';
 
+import Adapter from './adapters/adapter'
 import Player from './adapters/player';
 import Ghost from './adapters/ghost';
 
 const Adapters = {
   Player,
   Ghost,
+  Adapter
 };
 
 export {
